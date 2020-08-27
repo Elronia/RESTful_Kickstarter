b3a7bd8106fa9b7ad94dccb7fcbcdec2631b38cd
Elronia <zar.catherine@gmail.com>
2020-08-27 09:53:34 -0400

    edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,5 +19,32 @@ class ProjectsController < ApplicationController
         @project = Project.find(params[:id])
     end
 
+    def edit
+        # digesting the request: crompiling data for the form from the model
+        @project = Project.find(params[:id])
+        # response: render edit form view
+        render :edit
+      end
+    
+      # patch '/students/:id'
+    def update
+        # digesting the request: going through the params hash and checking in with the model to update a the instance
+        @project = Project.find(params[:id])
+        project_params = params.require(:project).permit(:title, :description)
+        @project.update(project_params)
+    
+        # response: redirect to the '/projects/:id', which will trigger the show action
+        redirect_to project_path(@project)
+    end
+    
+      # delete '/students/:id"
+    def destroy
+        # digesting the request: checking in with the model to find a project instance and destroy it
+        @project = Project.find(params[:id])
+        Project.destroy(params[:id])
+        #response: redirect to the index page
+        redirect_to projects_path
+    end
+    
 
 end
