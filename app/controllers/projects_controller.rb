2536a96ba543d1e4a2d84d6e4d787768f4de939b
Elronia <zar.catherine@gmail.com>
2020-08-28 23:56:35 -0400

    fixed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,10 +9,9 @@ class ProjectsController < ApplicationController
 
     def create
         # digesting the request: going through the params hash and checking in with the model to create a new instance
-        project_params = params.require(:project).permit(:title, :description)
         @project = Project.create(project_params)
         # response: redirect to the '/projects', which will trigger the index action
-        redirect_to projects_path
+        redirect_to project_path(@project)
     end
 
     def show
@@ -23,16 +22,13 @@ class ProjectsController < ApplicationController
         # digesting the request: crompiling data for the form from the model
         @project = Project.find(params[:id])
         # response: render edit form view
-        
-      end
+    end
     
       # patch '/students/:id'
     def update
         # digesting the request: going through the params hash and checking in with the model to update a the instance
         @project = Project.find(params[:id])
-        project_params = params.require(:project).permit(:title, :description)
         @project.update(project_params)
-    
         # response: redirect to the '/projects/:id', which will trigger the show action
         redirect_to project_path(@project)
     end
@@ -41,10 +37,13 @@ class ProjectsController < ApplicationController
     def destroy
         # digesting the request: checking in with the model to find a project instance and destroy it
         @project = Project.find(params[:id])
-        Project.destroy(params[:id])
+        @project.destroy
         #response: redirect to the index page
         redirect_to projects_path
     end
-    
-
+ 
+    private
+    def project_params
+        params.require(:project).permit(:title, :description)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+project1 = Project.create(title: "Meatballs", description: "Making dinner")
+project2 = Project.create(title: "Cake", description: "Decorating cake for Birthday")
+project3 = Project.create(title: "Apple Pie", description: "Indulgence")
+project4 = Project.create(title: "Earl Grey", description: "Drinking Tea")
+project5 = Project.create(title: "Milkshake", description: "Sweet and milky")
